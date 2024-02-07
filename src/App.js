b2729989b35eefc38ca472137d7b8daa3a683ebf
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2024-02-07 20:06:26 +0530

    add router dom modigfy a to Link and yheir text, wrap all in router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import PageWrapper from './components/PageWrapper';
-
+import Home from './components/Pages/Home';
 function App() {
   return (
-    <PageWrapper />
+    <Router>
+      <PageWrapper>
+        <Routes>
+          <Route
+            path='/'
+            Component={Home}
+          />
+        </Routes>
+      </PageWrapper>
+    </Router>
   );
 }
 
