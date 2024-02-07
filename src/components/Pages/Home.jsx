94fd3a18d4a2c692086237eb22ff1dee9c143ed3
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2024-02-07 21:53:12 +0530

    add services

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import Header from '../Common/Header';
 import image from "../assets/img/header-bg.jpg";
+
+// Reusable Components
+import Services from '../Common/Services';
 class Home extends Component {
     render() {
         return (
-            <Header
-                title="welcome To Our Studio!"
-                subtitle="IT's NICE TO MEET YOU"
-                buttonText="Tell me more"
-                link="/services"
-                showButton={true}
-                image={image}
-            />
+            <>
+                <Header
+                    title="welcome To Our Studio!"
+                    subtitle="IT's NICE TO MEET YOU"
+                    buttonText="Tell me more"
+                    link="/services"
+                    showButton={true}
+                    image={image}
+                />
+                <Services />
+            </>
         );
     }
 }
