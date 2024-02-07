0fc2d6eff0185000ef95df28389ccd450b4c4906
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2024-02-07 20:21:36 +0530

    add about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import PageWrapper from './components/PageWrapper';
+//pages
+import About from './components/Pages/About';
 import Home from './components/Pages/Home';
+
 function App() {
   return (
     <Router>
       <PageWrapper>
         <Routes>
           <Route
+            exact={true}
             path='/'
             Component={Home}
+          />
+          <Route
+            path='/about'
+            Component={About}
           />
         </Routes>
       </PageWrapper>
